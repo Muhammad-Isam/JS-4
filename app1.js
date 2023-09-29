4d02ecff4d07fb258b24e1c23ce00e1e284ee7b6
Muhammad-Isam <isam.boy@hotmail.com>
2023-09-29 15:19:01 +0500

    updated

--- app1.js
+++ app1.js
@@ -123,3 +123,86 @@
 // var string = num.toString().replace(".", "");
 // document.write("Number: "+num+"<br>");
 // document.write("Result: "+string);
+
+// QUESTION 13
+
+// var username = prompt("Enter your username: ");
+// for (var i=0; i<username.length; i++)
+// {
+//     if (username.charCodeAt(i)==33 || username.charCodeAt(i)==44 || username.charCodeAt(i)== 46 || username.charCodeAt(i)==64)
+//     {
+//         alert("Error! Please enter a valid username without special characters.");
+//     }
+// }
+
+// QUESTION 14
+
+// var A = ["cake", "apple pie", "cookie", "chips", "patties"];
+// var search = prompt("Enter an item to search: ");
+// search = search.toLowerCase();
+// var found=0;
+// for (var i=0; i<A.length; i++)
+// {
+//     if (search==A[i])
+//     {
+//         document.write(search+" is available at index "+i+" in our bakery.");
+//     }
+//     else {
+//         found++;
+//     }
+//     if (found==A.length)
+//     {
+//         document.write("We are sorry. "+search+" is <b>not available</b> in our bakery.");
+//     }
+// }
+
+// QUESTION 15
+
+// var password = prompt("Enter a password: ");
+// var alph = false, number = false;
+// var found = 0;
+// if (password[0] < 0 || password[0] >= 0) {
+//     document.write("Error! Password cannot begin with a number." + "<br>Please enter a valid password.");
+// }
+// if (password.length < 7) {
+//     document.write("Error! Password must be atleast 6 characters long!");
+// }
+// for (var i = 0; i < password.length; i++) {
+//     found++;
+//     if (password.charCodeAt(i) >= 48 && password.charCodeAt(i) <= 57) {
+//         number = true;
+//     } else if ((password.charCodeAt(i) >= 65 && password.charCodeAt(i) <= 90) || (password.charCodeAt(i) >= 97 && password.charCodeAt(i) <= 122)) {
+//         alph = true;
+//     }
+//     if (alph == true && number == true) {
+//         document.write("Password accepted!");
+//         break;
+//     }
+//     else if (found == password.length) {
+//         if (alph == false || number == false) {
+//             document.write("Error! Password must contain both alphabets and numbers!");
+//         }
+//     }
+// }
+
+// QUESTION 16
+
+// var string = "University of Karachi";
+// var splitString = string.split('');
+// for (var i=0; i<splitString.length; i++)
+// {
+//     document.write(splitString[i]+"<br>");
+// }
+
+// QUESTION 17
+
+// var input = prompt("Enter a string: ");
+// document.write("User input: "+input+"<br>");
+// document.write("Last character of input: "+input[input.length-1]);
+
+// QUESTION 18
+
+// var sentence = "The quick brown fox jumps over the lazy dog";
+// var find=/the/gi;
+// var occurrences=sentence.match(find);
+// document.write("Text: "+sentence+"<br>"+"There are "+occurrences.length+" occurrence(s) of word 'the'");
